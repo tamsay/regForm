119c34ac6112112b7d67dad2628d7ff12a2cb513
tamsay <longetemitope@gmail.com>
2020-05-29 18:10:24 +0100

    DevTask simple registration form with js validation alone

---
+++ script.js
@@ -0,0 +1,13 @@
+let registerBtn = document.querySelector('#registerBtn');
+
+let input = document.querySelectorAll('input')
+
+registerBtn.addEventListener('click', ()=>{
+    // input.forEach(element=>{
+    //     console.log(element)
+    //     if(element.textContent === ''){
+    //        // element.
+    //     }
+    // })
+    console.log(input)
+})
